93fda6f995037aa37951be30065ca3bbd81e55c8
dhanuka <dhanuka.g@civiten.com>
2018-03-23 03:33:25 +0000

    Revert "added library to device location on through app and location accuracy (pull request #6)"
    This reverts pull request #6.
    
    > added library to device location on through app and location accuracy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { LocationAccuracy } from '@ionic-native/location-accuracy';
 
 import { HomePage } from '../pages/home/home';
 @Component({
@@ -11,25 +10,12 @@ import { HomePage } from '../pages/home/home';
 export class MyApp {
   rootPage:any = HomePage;
 
-  constructor(
-		private locationAccuracy: LocationAccuracy,
-    platform: Platform, 
-    statusBar: StatusBar, 
-    splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
-      this.locationAccuracy.canRequest().then((canRequest: boolean) => {
-        if(canRequest) {
-          // the accuracy option will be ignored by iOS
-          this.locationAccuracy.request(this.locationAccuracy.REQUEST_PRIORITY_HIGH_ACCURACY).then(
-            () => console.log('Request successful'),
-            error => console.log('Error requesting location permissions', error)
-          );
-        }
-      });
     });
   }
 }
